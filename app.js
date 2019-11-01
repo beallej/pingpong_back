1644755e7f38e1b9d2de74bbd7a164f52d1c3eab
Josie Bealle <josiebealle@gmail.com>
2019-11-01 18:16:43 +0100

    change async await

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 var bodyParser  = require("body-parser");
 let fetch = require('node-fetch');
-const pg = require('pg');
+const { Client } = require('pg');
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const { Client } = require('pg');
+
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
@@ -21,7 +22,11 @@ app.post('/sendIp',async function(request, response){
     let location = await getLocation(ip);
     console.log("ip: " , ip);
     console.log("location: ", location);
-    let dbRes = await saveToDb(ip, location);
+    if (location.latitude && location.longitude){
+        let dbRes = await saveToDb(ip, location.latitude, location.longitude);
+    } else {
+        console.log("location info null for ip: ", ip)
+    }
     response.status = 200;
     return response
 
@@ -34,36 +39,30 @@ async function getLocation(ip) {
         + "?access_key=" + "938aa5bb84712b5de3034380f0b490d6"
         + "&fields=latitude,longitude";
     console.log(search);
-    fetch(search, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}})
-        .then(response => {
-        response.json()
-            .then((data) => {
-                console.log(data)
-            return {latitude: data.latitude, longitude: data.longitude}
-        })
-            .catch((err) => {
-                console.log(error)
-                return null
-            }
-        );
-    })
-        .catch(err => {
-            console.log(err);
-            return null;
+    try {
+        const response = await fetch(search, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
         });
+        const data = await response.json();
+        return {latitude: data.latitude, longitude: data.longitude}
+    } catch(err) {
+        console.log("error fetching location", err)
+        return null
+    }
+
 }
 
 const text = 'INSERT INTO IP_INFO(ADDRESS, LATITUDE, LONGITUDE) VALUES($1, $2, $3) RETURNING *';
-async function saveToDb(ip, location) {
-    const values = [ip, location.latitude, location.longitude]
+async function saveToDb(ip, latitude, longitude) {
+    const values = [ip, latitude, longitude]
     try {
         const res = await pool.query(text, values)
         console.log(res.rows[0])
         return res.rows[0];
     } catch (err) {
-        console.log(err.stack)
+        console.log("error saving to db", err.stack)
+        return null;
     }
 }
 
