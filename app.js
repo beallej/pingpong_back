f5ad2cb7425b4c1c1fe611fd250526a90151b55d
Josie Bealle <josiebealle@gmail.com>
2019-12-17 15:23:54 +0100

    stringify obj

--- app.js
+++ app.js
@@ -13,14 +13,11 @@ app.use(bodyParser.json());
 
 
 app.post('/traceroute',async function(request, response){
-    // console.log("TRACEROUTE" + request.body.src, request.body.traceroutes)
-    // console.log(IP_TYPES.USER)
     let src = await getInfoForIp(request.body.src, IP_TYPES.USER);
-    // console.log("src", src)
-
     let routes = await getTracerouteLocationInfo(src, request.body.traceroutes);
     console.log("routes: ", routes);
 
+    // let createResult = await addTraceroutesToDb([]);
     let createResult = await addTraceroutesToDb(routes);
     response.statusCode = 200;
     response.statusMessage = "Traceroutes successsfully added";
--- neo4jhelpers.js
+++ neo4jhelpers.js
@@ -12,13 +12,14 @@ async function addTraceroutesToDb(routes){
         createString += route.slice(0,-1).map((hop, index) => {
             return "(n" + index + ":IP " + JSON.stringify(hop) + ")-[:PINGS]->"
         }).join("");
-        let dst = route[-1];
+        let dst = route[route.length - 1];
         createString += "(n" + route.length -1 + ":IP " + JSON.stringify(dst) + ")"
         createString += " RETURN path";
         console.log(createString);
         return createString;
     }).join(", ");
-
+    console.log(createQueryString)
+    // let createQueryString = "MERGE path =(n0:IP {\"type\":\"USER\",\"address\":\"93.23.197.11\",\"latitude\":\"43.6046\",\"longitude\":\"1.4451\",\"asn\":\"AS15557\",\"isp\":\"SFR SA\"})-[:PINGS]->(n1:IP [{\"latitude\":null,\"longitude\":null,\"country_code\":null,\"asn\":null,\"isp\":null},{\"latitude\":null,\"longitude\":null,\"country_code\":null,\"asn\":null,\"isp\":null},{\"latitude\":null,\"longitude\":null,\"country_code\":null,\"asn\":null,\"isp\":null},{\"latitude\":null,\"longitude\":null,\"country_code\":null,\"asn\":null,\"isp\":null},{\"latitude\":null,\"longitude\":null,\"country_code\":null,\"asn\":null,\"isp\":null},{\"latitude\":48.8582,\"longitude\":2.3387,\"country_code\":\"FR\",\"asn\":\"AS15557\",\"isp\":\"SFR SA\"},{\"latitude\":48.8582,\"longitude\":2.3387,\"country_code\":\"FR\",\"asn\":\"AS15557\",\"isp\":\"SFR SA\"},{\"latitude\":48.8582,\"longitude\":2.3387,\"country_code\":\"FR\",\"asn\":\"AS15557\",\"isp\":\"SFR SA\"},{\"latitude\":48.8582,\"longitude\":2.3387,\"country_code\":\"FR\",\"asn\":\"AS15557\",\"isp\":\"SFR SA\"},{\"latitude\":48.8543,\"longitude\":2.3527,\"country_code\":\"FR\",\"asn\":\"AS15557\",\"isp\":\"SFR SA\"}])-[:PINGS]->NaN:IP undefined) RETURN path"
     let createResult = await session.run(createQueryString);
     session.close();
     return createResult
