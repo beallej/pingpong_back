69571487d0eb4684c96809ec320a3764ff37ee69
Josie Bealle <josiebealle@gmail.com>
2019-12-17 12:55:01 +0100

    f

--- ipLocateHelpers.js
+++ ipLocateHelpers.js
@@ -1,18 +1,24 @@
 const iplocate = require('node-iplocate');
 
+const ipLocateHelpers = (function() {
+    async function getLocation(ip) {
 
-async function getLocation(ip) {
-
-    iplocate(ip).then((results) => {
-        console.log(results);
-    });
-    try {
-        const data = await iplocate(ip);
-        return {latitude: data.latitude, longitude: data.longitude, country_code: data.country_code, asn: data.asn, isp: data.org}
-    } catch(err) {
-        console.log("error fetching location", err)
+        iplocate(ip).then((results) => {
+            console.log(results);
+        });
+        try {
+            const data = await iplocate(ip);
+            return {
+                latitude: data.latitude,
+                longitude: data.longitude,
+                country_code: data.country_code,
+                asn: data.asn,
+                isp: data.org
+            }
+        } catch (err) {
+            console.log("error fetching location", err)
+        }
+        return null
     }
-    return null
-}
+});
 
-export {getLocation};
