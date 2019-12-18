d416503ab034a03340bf148490d9f2e2e083a6b9
Josie Bealle <josiebealle@gmail.com>
2019-12-18 12:35:55 +0100

    ff

--- neo4jhelpers.js
+++ neo4jhelpers.js
@@ -1,12 +1,12 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase(process.env['GRAPHENEDB_URL']);
 async function addTraceroutesToDb(routes){
-    let queries = []
+
+
+    let nodeQueries = []
     routes.map((route) => {
-        let src = route[0].address;
-        let dst = route[route.length -1].address;
         route.map((hop) => {
-            queries.push({
+            nodeQueries.push({
                 query: "MERGE (ip:IP {address: {ipAddress}, latitude: {ipLatitude}, longitude: {ipLongitude}, asn: {ipAsn}, isp: {ipIsp}}) RETURN ip",
                 params: {
                     ipAddress: hop.address,
@@ -18,35 +18,55 @@ async function addTraceroutesToDb(routes){
                 lean: true
             })
         });
-        route.map((hop, ind) => {
-            if (ind < route.length - 1) {
-                queries.push({
-                    query: "MATCH (node1: IP), (node2: IP) " +
-                        "WHERE node1.address = {node1Address} AND node2.address = {node2Address} " +
-                        "MERGE (node1)-[r:PINGS {src: {srcIp}, dst: {dstIp}}]->(node2) RETURN r",
-                    params: {
-                        node1Address: hop.address,
-                        node2Address: route[ind + 1].address,
-                        srcIp: src,
-                        dstIp: dst
-                    },
-                    lean: true
-                })
-            }
-        })
-        console.log("QUERY", queries)
+        console.log("PING - QUERY", nodeQueries)
     });
-
     db.cypher({
-        queries: queries,
+        queries: nodeQueries,
     }, (err, batchResults) => {
         if (batchResults){
-            console.log("RESULTS", JSON.stringify(batchResults))
+            console.log("RESULTS - IP", JSON.stringify(batchResults))
+            addPingRelationships()
         }
         if (err){
             console.log("ERR", err)
         }
     });
+
+    function addPingRelationships() {
+        let relQueries = [];
+        routes.map((route) => {
+            let src = route[0].address;
+            let dst = route[route.length -1].address;
+            route.map((hop, ind) => {
+                if (ind < route.length - 1) {
+                    relQueries.push({
+                        query: "MATCH (node1: IP), (node2: IP) " +
+                            "WHERE node1.address = {node1Address} AND node2.address = {node2Address} " +
+                            "MERGE (node1)-[r:PINGS {src: {srcIp}, dst: {dstIp}}]->(node2) RETURN r",
+                        params: {
+                            node1Address: hop.address,
+                            node2Address: route[ind + 1].address,
+                            srcIp: src,
+                            dstIp: dst
+                        },
+                        lean: true
+                    })
+                }
+            })
+        })
+        console.log("REL - QUERIES", relQueries);
+        db.cypher({
+            queries: relQueries,
+        }, (err, batchResults) => {
+            if (batchResults){
+                console.log("RESULTS - PING", JSON.stringify(batchResults))
+            }
+            if (err){
+                console.log("ERR", err)
+            }
+        });
+    }
+
 
 }
 
