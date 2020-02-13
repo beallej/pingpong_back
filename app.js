611b649bb7d081591c9702225ee17cbc42be7945
Josie Bealle <josiebealle@gmail.com>
2020-02-13 20:55:21 +0100

    increase payload size

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser  = require("body-parser");
 
 
 const app = express();
-app.use(bodyParser.json({ limit: "50mb" }))
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 
 /*** Add information from execution of script to gather traceroutes.
